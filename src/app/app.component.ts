a8f6285b991725d1a29008c08bd9e03b155f090d
Abbes Yassine <abbesyassine4@gmail.com>
2019-03-08 20:28:04 +0100

    fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Appareil} from './appareil';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  title = 'Hello';
+
+  isAuth: boolean = false;
+
+  appareil: Appareil = new Appareil();
+
+
+  appareilOne: string = 'Iphone';
+  appareilTwo: string = 'Ordinateur';
+  appareilThree: string = 'Frigo';
+
+  lastUpdate: Date = new Date();
+
+  appareils: Appareil[] = [];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      },
+      4000
+    );
+
+    this.appareils = [
+      {
+        name: 'Iphone',
+        status: true
+      },
+      {
+        name: 'Ordinateur',
+        status: false
+      },
+      {
+        name: 'Frigo',
+        status: true
+      }
+    ];
+  }
+
+  onSubmit() {
+    console.log(this.appareil);
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
+
+
 }
